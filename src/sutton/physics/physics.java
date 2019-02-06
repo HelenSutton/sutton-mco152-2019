bf65aa98f2d5ca9b0fbeb91baf8b9a6d606ab7e1
Helen <helensutton97@gmail.com>
2019-02-05 20:20:20 -0500

    changed max min values to constants

--- src/sutton/physics/physics.java
+++ src/sutton/physics/physics.java
@@ -7,18 +7,18 @@ public class physics {
     public static void main(String [ ] args) {
 
          ArrayList<Projectile> plist = new ArrayList<Projectile>();
+         final int maxVelocity = 120;
+         final int minVelocity = 50;
+         final int maxDegrees = 150;
+         final int minDegrees = 30;
 
          for (int i = 0; i <= 120; i ++){
              plist.forEach((n) -> n.addTime(1));
              Random r = new Random();
              int randomAmount = r.nextInt(6);
              for(int j = 0; j < randomAmount; j++){
-                 int max = 120;
-                 int min = 50;
-                 int velocity = r.nextInt((max - min) + 1) + min;
-                 max = 150;
-                 min = 30;
-                 int degrees = r.nextInt((max - min) + 1) + min;
+                 int velocity = r.nextInt((maxVelocity - minVelocity) + 1) + minVelocity;
+                 int degrees = r.nextInt((maxDegrees - minDegrees) + 1) + minDegrees;
                  Projectile temp = new Projectile (velocity, degrees);
                  plist.add(temp);
              }
