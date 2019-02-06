82b40463926cd656f00cf00b11f83aadd1c12734
Helen <helensutton97@gmail.com>
2019-02-05 20:07:02 -0500

    edited code based on comments

--- src/sutton/physics/physics.java
+++ src/sutton/physics/physics.java
@@ -10,17 +10,23 @@ public class physics {
 
          for (int i = 0; i <= 120; i ++){
              plist.forEach((n) -> n.addTime(1));
-             int randomAmount = new Random().nextInt(6);
+             Random r = new Random();
+             int randomAmount = r.nextInt(6);
              for(int j = 0; j < randomAmount; j++){
-                 int velocity = new Random().nextInt((120 - 50) + 1) + 50;
-                 int degrees = new Random().nextInt((150 - 30) + 1) + 30;
+                 int max = 120;
+                 int min = 50;
+                 int velocity = r.nextInt((max - min) + 1) + min;
+                 max = 150;
+                 min = 30;
+                 int degrees = r.nextInt((max - min) + 1) + min;
                  Projectile temp = new Projectile (velocity, degrees);
                  plist.add(temp);
              }
          }
         for (Projectile n : plist) {
-            if (n.getY() > 0)
+            if (n.getY() > 0) {
                 System.out.println(n);
+            }
         }
 
     }
