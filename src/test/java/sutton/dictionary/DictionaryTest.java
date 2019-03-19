769f6f18fd5c961a442045fb46e96f0050666083
Helen <helensutton97@gmail.com>
2019-03-19 18:17:40 -0400

    more bananas

--- src/test/java/sutton/dictionary/DictionaryTest.java
+++ src/test/java/sutton/dictionary/DictionaryTest.java
@@ -14,21 +14,6 @@ class DictionaryTest {
         assertTrue(dictionary.contains("Wonderful"));
     }
 
-    @Test
-    public void isPalindrome_true() throws FileNotFoundException{
-        Dictionary dictionary = new Dictionary();
-
-
-        assertTrue(dictionary.isPalindrome("racecar"));
-    }
-
-    @Test
-    public void isPalindrome_false() throws FileNotFoundException{
-        Dictionary dictionary = new Dictionary();
-
-
-        assertFalse(dictionary.isPalindrome("abdfba"));
-    }
 
     @Test
     public void contains_false() throws FileNotFoundException{
--- src/test/java/sutton/dictionary/PalindromeTest.java
+++ src/test/java/sutton/dictionary/PalindromeTest.java
@@ -1,5 +1,26 @@
+package sutton.dictionary;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromeTest {
+    @Test
+    public void isPalindrome_true() throws FileNotFoundException {
+        Palindrome palindrome = new Palindrome();
+
+
+        assertTrue(palindrome.isPalindrome("racecar"));
+    }
+
+    @Test
+    public void isPalindrome_false() throws FileNotFoundException{
+        Palindrome palindrome = new Palindrome();
+
+
+        assertFalse(palindrome.isPalindrome("abdfba"));
+    }
 
 }
