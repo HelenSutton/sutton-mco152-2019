9f4b8c21e9ab79cd8f40f5dbabfc21e72a51ffb2
Helen <helensutton97@gmail.com>
2019-03-24 16:52:26 -0400

    added new graphics to golf

---
+++ src/main/java/sutton/golf/GolfCourseComponent.java
@@ -0,0 +1,70 @@
+package sutton.golf;
+
+import sutton.physics.Projectile;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GolfCourseComponent extends JComponent {
+    double x = 0.0;
+    Projectile ball = new Projectile(1,90);
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        int ground = getHeight()*2/3;
+
+        //draw sky
+        graphics.setColor(Color.cyan);
+        graphics.fillRect(0, 0, getWidth(), getHeight());
+
+        //draw sun
+        graphics.setColor(Color.yellow);
+        graphics.fillOval(getWidth()-60,30,40,40 );
+
+        //background mountains
+
+
+        //draw ground
+        graphics.setColor(Color.green);
+        graphics.fillRect(0, ground, getWidth(), getHeight());
+
+        //more ground
+        Color brown = new Color(100,50,0);
+        graphics.setColor(brown);
+        graphics.fillRect(0, getHeight()*11/15, getWidth(), getHeight());
+        Color lightbrown =new Color (130,90,70);
+        graphics.setColor(lightbrown);
+        graphics.fillRect(0, (getHeight()*12/15), getWidth(), getHeight());
+        graphics.setColor(brown);
+        graphics.fillRect(0, getHeight()*13/15, getWidth(), getHeight());
+        graphics.setColor(lightbrown);
+        graphics.fillRect(0, getHeight()*14/15, getWidth(), getHeight());
+
+
+
+
+        //draww ball
+
+        graphics.setColor(Color.white);
+        double ballX=ball.getX()+30;
+        double ballY = (ground-15)+ball.getY();
+
+        graphics.fillOval((int) (ballX), (int) (ballY),15,15);
+        ball.addTime(.02);
+
+        //draw flagg pole
+        int poleheight = 40;
+        int poleplacement = 700;
+        graphics.setColor(Color.gray);
+        graphics.fillRect(poleplacement, ground-poleheight, 3,poleheight);
+
+        //draw flag
+        int flagheight =14;
+        int flaglength =15;
+        graphics.setColor(Color.red);
+        graphics.fillPolygon(new int[] {poleplacement-flaglength,poleplacement,poleplacement}, new int[] {ground-poleheight+flagheight/2,ground-poleheight,ground-poleheight+flagheight},3);
+
+        repaint();
+
+    }
+}
--- src/main/java/sutton/golf/GolfFrame.java
+++ src/main/java/sutton/golf/GolfFrame.java
@@ -1,39 +1,24 @@
 package sutton.golf;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+
 
 public class GolfFrame extends JFrame{
 
     public GolfFrame(){
-        setTitle("TOuro Golf");
-        setSize(600,400);
+        setTitle("Touro Golf");
+        setSize(800,400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JPanel root = new JPanel();
         root.setLayout(new BorderLayout());
 
-        JLabel north = new JLabel(new ImageIcon("im-batman.jpg"));
-        north.setBackground(Color.pink);
-        north.setOpaque(true);
-        root.add(north, BorderLayout.NORTH);
 
-        JLabel south = new JLabel("SOUTH");
-        south.setBackground(Color.magenta);
-        south.setOpaque(true);
-        root.add(south, BorderLayout.SOUTH);
+        GolfCourseComponent  golfCorseComponent = new GolfCourseComponent();
+        root.add(golfCorseComponent, BorderLayout.CENTER);
 
-        JLabel west = new JLabel("WEST");
-        west.setBackground(Color.cyan);
-        west.setOpaque(true);
-        root.add(west, BorderLayout.WEST);
-
-        JButton center = new JButton("CENTER");
-        center.setBackground(Color.lightGray);
-        center.setOpaque(true);
-
-        center.addActionListener(this::printClick);
-        root.add(center, BorderLayout.CENTER);
 
         setContentPane(root);
     }
